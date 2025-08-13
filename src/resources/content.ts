9081aaae148fce25cb7b995d7c41029e45c104da
Maha Aryaman Singh (nickname- ashish) <ashish.js05263@gmail.com>
2025-08-13 19:46:02 +0530

    working 1

--- src/resources/content.ts
+++ src/resources/content.ts
@@ -13,17 +13,6 @@ const person = {
   languages: ["English", "Hindi"], // optional: Leave the array empty if you don't want to display languages
 };
 
-const newsletter = {
-  display: true,
-  title: <>Subscribe to {person.firstName}'s Newsletter</>,
-  description: (
-    <>
-      I occasionally write about design, technology, and share thoughts on the intersection of
-      creativity and engineering.
-    </>
-  ),
-};
-
 const social = [
   // Links are automatically displayed.
   // Import new icons in /once-ui/icons.ts
