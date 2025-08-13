c15f8acbc10395ff8caeba6094a68923910c0e66
Maha Aryaman Singh (nickname- ashish) <ashish.js05263@gmail.com>
2025-08-13 15:06:27 +0530

    Chnages made
    Added Personal details

---
+++ src/resources/once-ui.config.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: "Ashish Kumar Singh",
+  description: "Freelancer | Tech Enthusiast | Educational Leader",
+  socialLinks: {
+    github: "https://github.com/ashish01-dev",
+    linkedin: "https://www.linkedin.com/in/ashish-kumar-singh-9002a437a/",
+    email: "ashish.jayshreeran@gmail.com",
+    location: "New Delhi, India"
+  },
+} as const;
