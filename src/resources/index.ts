0d7e497cb5c67a013b09c61686e3e2ed653de930
Maha Aryaman Singh (nickname- ashish) <ashish.js05263@gmail.com>
2025-08-13 18:00:42 +0530

    Update 49

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -10,3 +10,5 @@ export {
 } from "./content";
 
 export { display, mailchimp, routes, protectedRoutes, baseURL, fonts, style, schema, sameAs, effects, dataStyle } from "./once-ui.config";
+// --- Export content ---
+export { person, social, home, about, work, blog, gallery } from "./content";
