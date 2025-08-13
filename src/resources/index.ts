c3c0eb3f19cb7baf7a021f1fdd6be2776a13d048
Maha Aryaman Singh (nickname- ashish) <ashish.js05263@gmail.com>
2025-08-13 17:23:43 +0530

    Update 32

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -1,25 +1,13 @@
-// src/resources/index.ts
-
-// Re-export items from content.ts that actually exist
+// Export content
 export {
   person,
   social,
   home,
   about,
-  // blog, work, gallery removed since they don't exist in content.ts
+  blog,
+  work,
+  gallery,
 } from "./content";
 
-// Re-export config and utilities
-export {
-  display,
-  mailchimp,
-  routes,
-  protectedRoutes,
-  baseURL,
-  fonts,
-  style,
-  schema,
-  sameAs,
-  effects,
-  dataStyle,
-} from "./once-ui.config";
+// Export config
+export { display, mailchimp, routes, protectedRoutes, baseURL, fonts, style, schema, sameAs, effects, dataStyle } from "./once-ui.config";
