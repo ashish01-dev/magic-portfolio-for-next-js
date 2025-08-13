1f67db99e061cd8519377b0b7a1e921c18ac1600
Maha Aryaman Singh (nickname- ashish) <ashish.js05263@gmail.com>
2025-08-13 19:38:34 +0530

    improving 5

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,17 @@
-// src/app/blog/page.tsx
-import React from "react";
-import { Column, Heading, Schema } from "@once-ui-system/core";
-import { blog, person, baseURL } from "@/resources";
+import { Column, Heading, Meta, Schema } from "@once-ui-system/core";
+import { Mailchimp } from "@/components";
 import { Posts } from "@/components/blog/Posts";
+import { baseURL, blog, person, newsletter } from "@/resources";
+
+export async function generateMetadata() {
+  return Meta.generate({
+    title: blog.title,
+    description: blog.description,
+    baseURL: baseURL,
+    image: `/api/og/generate?title=${encodeURIComponent(blog.title)}`,
+    path: blog.path,
+  });
+}
 
 export default function Blog() {
   return (
@@ -20,10 +29,16 @@ export default function Blog() {
           image: `${baseURL}${person.avatar}`,
         }}
       />
-      <Heading marginBottom="l" variant="display-strong-s">{blog.title}</Heading>
-      <Posts range={[1, 1]} thumbnail direction="column" />
-      <Posts range={[2, 3]} thumbnail />
-      <Posts range={[4]} columns="2" />
+      <Heading marginBottom="l" variant="display-strong-s">
+        {blog.title}
+      </Heading>
+      <Column
+				fillWidth flex={1}>
+				<Posts range={[1,1]} thumbnail direction="column"/>
+				<Posts range={[2,3]} thumbnail/>
+				<Posts range={[4]} columns="2"/>
+			</Column>
+      {newsletter.display && <Mailchimp newsletter={newsletter} />}
     </Column>
   );
 }
