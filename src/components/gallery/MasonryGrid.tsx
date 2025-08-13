f21156495e4ceb93de5faf31986f2e305f55697c
Maha Aryaman Singh (nickname- ashish) <ashish.js05263@gmail.com>
2025-08-13 16:38:01 +0530

    Update 19

--- src/components/gallery/MasonryGrid.tsx
+++ src/components/gallery/MasonryGrid.tsx
@@ -3,7 +3,23 @@
 import Masonry from "react-masonry-css";
 import { Media } from "@once-ui-system/core";
 import styles from "./Gallery.module.scss";
-import { gallery } from "@/resources";
+
+type GalleryImage = {
+  src: string;
+  alt: string;
+  orientation?: "horizontal" | "vertical";
+};
+
+const gallery = {
+  images: [
+    { src: "/images/placeholder1.jpg", alt: "Gallery Image 1", orientation: "horizontal" },
+    { src: "/images/placeholder2.jpg", alt: "Gallery Image 2", orientation: "vertical" },
+    { src: "/images/placeholder3.jpg", alt: "Gallery Image 3", orientation: "horizontal" },
+    { src: "/images/placeholder4.jpg", alt: "Gallery Image 4", orientation: "vertical" },
+    { src: "/images/placeholder5.jpg", alt: "Gallery Image 5", orientation: "horizontal" },
+    { src: "/images/placeholder6.jpg", alt: "Gallery Image 6", orientation: "vertical" }
+  ] as GalleryImage[]
+};
 
 export default function MasonryGrid() {
   const breakpointColumnsObj = {
@@ -17,15 +33,15 @@ export default function MasonryGrid() {
       className={styles.masonryGrid}
       columnClassName={styles.masonryGridColumn}
     >
-      {gallery.images.map((image, index) => (
+      {gallery.images.map((image: GalleryImage, index: number) => (
         <Media
-          priority={index < 10}
-          sizes="(max-width: 560px) 100vw, 50vw"
           key={index}
           radius="m"
           aspectRatio={image.orientation === "horizontal" ? "16 / 9" : "3 / 4"}
           src={image.src}
           alt={image.alt}
+          priority={index < 10}
+          sizes="(max-width: 560px) 100vw, 50vw"
           className={styles.gridItem}
         />
       ))}
